7ad752aa97a16c95c7d1fc7f55e2a27e101a4aea
sanankhan <sananWEB>
2022-03-23 11:10:04 +0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,75 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import Loading from "./components/loading"  
+import SignIn from "./components/signin"   
+import SignUp from "./components/signup"
+import {BrowserRouter,Route,Routes,Link,useHistory,useRouteMatch,Redirect,useParams,useLocation, Switch} from "react-router-dom"
+import { createTheme ,ThemeProvider, responsiveFontSizes } from '@material-ui/core/styles';
+import Homepage from "./components/homepage"
+
+
+let theme = createTheme ({
+  palette: {
+    primary: {
+    
+      main: '#0A5995',
+      
+    },
+  },
+  
+});
+
+
+theme = responsiveFontSizes(theme);
+
+
+
+function App() {
+
+  const [loading,setloading] = useState(true)  
+
+
+  useEffect(() => {
+  
+    setTimeout(() => {
+      setloading(false);
+    }, 2000);
+  }, []);
+
+if(loading==true) {
+
+  return (
+  <BrowserRouter>
+   <Routes>
+    <Route  path="/" element={<Loading/>} />
+    </Routes>
+    </BrowserRouter>
+    )
+  
+}  
+else{
+
+  return (
+
+    <>
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+    <Routes>
+    
+    <Route exact path="/" element={<SignIn/>} />
+    <Route path="/signup" element ={<SignUp/>} />
+    <Route path="/homepage" element ={<Homepage/>} />
+
+    </Routes>
+    </ThemeProvider>
+    </BrowserRouter>
+    {/* <SignIn/>
+    <SignUp/> */}
+    </>
+  );
+
+}
+  
+}
+
+export default App;
---
+++ frontend/src/components/signin.js
@@ -0,0 +1,200 @@
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import MuiAlert from '@material-ui/lab/Alert'
+import {Snackbar } from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import axios from "axios"
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Link,
+  useNavigate,
+  useRouteMatch,
+  Redirect,
+  useParams,
+  useLocation,
+  Switch,
+} from "react-router-dom";
+import logo from "./logo.png";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+
+  const [msg, setmsg] = useState(null);
+  const [open, setopen] = useState(false);
+  const [data, setdata] = useState({
+    email: "",
+    password: "",
+  });
+
+  const change = (e) => {
+    setdata({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const history=useNavigate()
+  const submit = (e) => {
+    e.preventDefault();
+
+    console.log(data);
+
+    axios
+      .post("/signin", data)
+      .then((res) => {
+
+
+        console.log(res.data.msg)
+        setmsg(res.data.msg);
+          setopen(true);
+        if (res.data.msg == "log In Successful") {
+          setmsg(res.data.msg);
+          setopen(true);
+          history("/homepage");
+        } else {
+         
+          
+          setmsg(res.data.msg);
+          setopen(true);
+        
+        }
+      });
+  };
+
+  const handleClick = () => {
+    setopen(false);
+  };
+
+
+  
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <img src={logo} style={{ width: 90 }} />
+
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form onSubmit={submit} className={classes.form}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            value={data.email}
+            onChange={change}
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            type="email"
+            // inputProps={{ pattern: "" }}
+
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={data.password}
+            onChange={change}
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link
+                to="/signup"
+                variant="body2"
+                style={{ color: "#0A5995", textDecoration: "none" }}
+              >
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+
+      <Snackbar open={open} autoHideDuration={3000} onClose={handleClick} >
+
+
+{(()=>{
+
+if(msg=="Incorrent Password"){
+  return(
+    <MuiAlert  severity="error" elevation={6} variant="filled">
+    {msg}
+    </MuiAlert>
+  )
+
+}
+else if(msg=="This Email is not registered"){
+  return(
+    <MuiAlert  severity="error" elevation={6} variant="filled">
+    {msg}
+    </MuiAlert>
+  )
+}
+
+else{
+return(
+  <MuiAlert  severity="success" elevation={6} variant="filled">
+{msg}
+</MuiAlert>
+)
+}
+})()}
+
+ 
+
+
+
+</Snackbar>
+    </Container>
+  );
+}
